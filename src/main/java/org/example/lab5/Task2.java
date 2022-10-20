72a03671bb9f4b8221729889e7992e983b1dea46
Gurbanov Tamerlan <t64566t@gmail.com>
2022-10-20 23:15:06 +0300

    fix(project) wrong neighbours
    Fixes wrong neighbour display due to ignore comm_cart.Shift .

--- src/main/java/org/example/lab5/Task2.java
+++ src/main/java/org/example/lab5/Task2.java
@@ -7,52 +7,51 @@ import static mpi.Cartcomm.*;
 
 public class Task2 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         MPI.Init(args);
 
         // Size of the default communicator
         int size = MPI.COMM_WORLD.Size();
-        if (size != 4)
-        {
+        if (size != 4) {
             System.out.printf("This application is meant to be run with 4 processes, not %d.\n", size);
         }
 
         // Ask MPI to decompose our processes in a 2D cartesian grid for us
-        int dims[] = { 0, 0 };
-        Dims_create(size,  dims);
+        int[] dims = {0, 0};
+        Dims_create(size, dims);
 
         // Make both dimensions non-periodic
-        boolean periods[] = { false, false };
+        boolean[] periods = {false, false};
 
         // Let MPI assign arbitrary ranks if it deems it necessary
         boolean reorder = true;
         Cartcomm comm_cart = MPI.COMM_WORLD.Create_cart(dims, periods, reorder);
 
 
-
         // Declare our neighbours
-        String neighbours_names[] = { "down", "up", "left", "right" };
-        int neighbours_ranks[]= new int[4];
+        String[] neighbours_names = {"down", "up", "left", "right"};
+        int[] neighbours_ranks = new int[4];
 
         // Let consider dims[0] = X, so the shift tells us our left and right neighbours
-        comm_cart.Shift(0, 1);
-
-
+        var cordX = comm_cart.Shift(0, 1);
+        neighbours_ranks[2] = cordX.rank_source;
+        neighbours_ranks[3] = cordX.rank_dest;
         // Let consider dims[1] = Y, so the shift tells us our up and down neighbours
-        comm_cart.Shift(1, 1);
-
+        var cordY = comm_cart.Shift(1, 1);
+        neighbours_ranks[0] = cordY.rank_source;
+        neighbours_ranks[1] = cordY.rank_dest;
         // Get my rank in the new communicator
         int my_rank = comm_cart.Rank();
-        int coordinates[] = comm_cart.Coords(my_rank);
-        System.out.printf("Process rank %d has coordinates %d %d\n",
-                my_rank, coordinates[0], coordinates[1]);
-
-        for (int i = 0; i < 4; i++)
-        {
-            if (neighbours_ranks[i] == MPI.PROC_NULL)
+        int[] coordinates = comm_cart.Coords(my_rank);
+        Thread.sleep(100*(my_rank+1));
+        System.out.printf("Process rank %d has coordinates %d %d\n", my_rank, coordinates[0], coordinates[1]);
+        for (int i = 0; i < 4; i++) {
+            if (neighbours_ranks[i] <= 0) {
                 System.out.printf("[MPI process %d] I have no %s neighbour.\n", my_rank, neighbours_names[i]);
-            else
-                System.out.printf("[MPI process %d] I have a %s neighbour: process %d.\n", my_rank, neighbours_names[i], neighbours_ranks[i]);
+            } else {
+                System.out.printf("[MPI process %d] I have a %s neighbour: process %d.\n", my_rank, neighbours_names[i],
+                        neighbours_ranks[i]);
+            }
         }
 
         MPI.Finalize();
